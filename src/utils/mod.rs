0f9815b4d43341c0e8273f901bc46e737f58fdfe
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2025-12-11 23:57:52 +0530

    fixed run_bot function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,8 @@
+use crate::jupiter_client::JupiterClient;
+
 pub async fn run_bot() {
     println!("Bot is running");
+    let client = JupiterClient::new();
+    println!("{}", client.base_url);
+    
 }
