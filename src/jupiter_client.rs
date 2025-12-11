68bf2fd2d9a704f0f2c624156130b0104f115a7d
KAUSTUBHOG <kaustubh.devlop@gmail.com>
2025-12-11 23:55:45 +0530

    changed jupiter-Client.rs

--- src/jupiter_client.rs
+++ src/jupiter_client.rs
@@ -1,7 +1,15 @@
-pub struct Jupiterclient;
+pub struct JupiterClient {
+    pub http_client: reqwest::Client,
+    pub base_url: String,
+}
 
-impl Jupiterclient {
-    pub fn new() -> self {
-        Jupiterclient
+impl JupiterClient {
+    pub fn new() -> Self {
+        let client = reqwest::Client::new();
+
+        Self {
+            http_client: client,
+            base_url: "https://quote-api.jup.ag/v6".to_string(),
+        }
     }
 }
